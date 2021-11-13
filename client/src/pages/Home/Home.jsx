2d935dea7e580e01e12457652d99104b7255ee21
robester0403 <robkso@gmail.com>
2021-11-13 08:55:00 -0500

    Linked cancel button, removed onSelect, deleted JSON client data

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -65,7 +65,6 @@ class Home extends Component {
           </article>
           <article className="main-content__next-vid-box">
             {this.state.videos && <NextList
-              onVideoSelect={this.state.selectedVideo}
               nextVideos={filteredList}
             /> }
           </article>
